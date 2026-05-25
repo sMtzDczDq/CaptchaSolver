7a0cc385b4d203297de6c5bf8943e02a803e8805
sMtzDczDq <75165692+sMtzDczDq@users.noreply.github.com>
2026-05-25 13:25:55 +0200

    update deno

--- solver/bash.sh
+++ solver/bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -x
 ./checkdeps.sh
 sleep 1
-node ./ocr.js keep2share.cc "$1" >> log.txt 2>&1
+/usr/local/bin/deno --allow-run --allow-read --allow-write --allow-env ./ocr.js keep2share.cc "$1" >> log.txt 2>&1
